5663830bc9524015a43d09a18e702bdebb9aba27
PrasannaKumar-Git <prasannakumar.mail02@gmail.com>
2025-08-21 11:37:16 +0530

    Fix tracking of dist folder after removing nested repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portifolio/', // Set this to your GitHub repository name
+  base: "/PrasannaKumarPortifolio/", // Set this to your GitHub repository name
 });
